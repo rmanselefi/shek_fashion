e5c6adc1c2a95762836ccddb4ec694003ea42775
Razin <kebederaz@gmail.com>
2021-01-27 14:06:48 +0300

    just build

--- src/store/actions/salesAction.ts
+++ src/store/actions/salesAction.ts
@@ -4,55 +4,65 @@ import { Sales } from "../../models/sales";
 export const registerSales = (sales: Sales) => {
   return async (dispatch: any, getState: any): Promise<Sales | null> => {
     // const fb = getFirebase();
-    var res = await firebase
+    try {
+      console.log("salessalessalessales", sales);
+
+      var res = await firebase
         .firestore()
         .collection("product")
         .doc(sales.productid)
         .get();
       var name = res.data()?.name;
-      var brand=res.data()?.brand;
-      var prod={
-        id:sales.productid,
-        name:name,
+      var brand = res.data()?.brand;
+      var color = res.data()?.color;
+      var prod = {
+        id: sales.productid,
+        name: name,
         brand,
-      }
-      if (res.data()?.stock<sales.quantity) {
-        console.log('====================================');
-        console.log('jhgvjhgvhgvhgchgfcvcgfcghcghfc');
-        console.log('====================================');
+        color,
+      };
+      if (Number(res.data()?.stock) < Number(sales.quantity)) {
+        console.log("====================================");
+        console.log(sales.quantity);
+        console.log("====================================");
         return null;
-      }    
-      var cashier={
-        id:sales.cashierid,
-        name:sales.cashier
       }
-    var resp = await firebase.firestore().collection("sales").add({
-      product: prod,
-      price: sales.price,
-      quantity: sales.quantity,
-      branch:sales.branch,
-      cashier,
-      soldby:sales.soldby,
-      sellerid:sales.sellerid,
-      createdAt: new Date(),
-    });
-    if (resp != null) {
-      var response = await firebase
-        .firestore()
-        .collection("product")
-        .doc(sales.productid)
-        .get();
-      var stock = response.data()?.stock;
-      var remaining = stock - sales.quantity;
-      await firebase
-        .firestore()
-        .collection("product")
-        .doc(sales.productid)
-        .update({
-          stock: remaining,
-        });
-      return sales;
-    } else {
+      var cashier = {
+        id: sales.cashierid,
+        name: sales.cashier,
+      };
+      var resp = await firebase.firestore().collection("sales").add({
+        product: prod,
+        price: sales.price,
+        quantity: sales.quantity,
+        branch: sales.branch,
+        cashier,
+        soldby: sales.soldby,
+        sellerid: sales.sellerid,
+        createdAt: new Date(),
+      });
+
+      if (resp != null) {
+        var response = await firebase
+          .firestore()
+          .collection("product")
+          .doc(sales.productid)
+          .get();
+        var stock = response.data()?.stock;
+        var remaining = stock - sales.quantity;
+        await firebase
+          .firestore()
+          .collection("product")
+          .doc(sales.productid)
+          .update({
+            stock: remaining,
+          });
+        return sales;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log("errorerror", error);
       return null;
     }
   };
@@ -69,9 +79,9 @@ export const updateSales = (sales: Sales) => {
         product: sales.productid,
         price: sales.price,
         quantity: sales.quantity,
-        branch:sales.branch,
-        soldby:sales.soldby,
-        sellerid:sales.sellerid,
+        branch: sales.branch,
+        soldby: sales.soldby,
+        sellerid: sales.sellerid,
         updatedAt: new Date(),
       });
     if (resp != null) {
